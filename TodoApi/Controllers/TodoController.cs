0ad0add2abe6a21490918adbb97312ae4ab66c38
PC <julya.p.dongmail.com>
2019-02-01 17:57:45 +0200

    Initial commit

---
+++ TodoApi/Controllers/TodoController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using TodoApi.Models;
+
+namespace TodoApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TodoController : ControllerBase
+    {
+        private readonly TodoContext _context;
+        
+        public TodoController(TodoContext context)
+        {
+            _context = context;
+
+            if(_context.TodoItems.)
+            {
+
+            }
+        }
+    }
+}
